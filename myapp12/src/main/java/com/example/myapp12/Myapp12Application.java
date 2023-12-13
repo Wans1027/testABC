c3d55fddf5136e7b207486fc71b2bb4c6cad4002
wans <rladhkstmd10@hanmail.net>
2023-12-13 14:59:04 +0900

    first Commit

---
+++ myapp12/src/main/java/com/example/myapp12/Myapp12Application.java
@@ -0,0 +1,13 @@
+package com.example.myapp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp12Application.class, args);
+	}
+
+}
---
+++ myapp12/src/test/java/com/example/myapp12/Myapp12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myapp12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myapp12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
